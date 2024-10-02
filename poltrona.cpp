508d54fc44686fae5fc4bf37ac241c682545683f
Nicolas Nobre <165395180+NicolasNobre05@users.noreply.github.com>
2024-10-02 16:05:22 -0300

    Update poltrona.cpp

--- poltrona.cpp
+++ poltrona.cpp
@@ -3,39 +3,37 @@
 #include <cstdlib>
 #include <ctype.h>
 
-int main() {
-    char fileira;
-    int col;
-    int poltrona;
-    char fileirasPermitidas[] = {'A','B', 'C', 'D', 'E'};
-    char continuar = 'S';
 
-    char matriz[5][5] = {
-        {'0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '0'}
+char fileira;
+int col;
+int poltrona;
+char fileirasPermitidas[] = {'A','B', 'C', 'D', 'E', 'F'};
+char continua = 'S';
+bool onibusCheio;
+int classeReserva;
+
+char matriz[5][6] = {
+        {'0', '0', '0', '0', '0', '0'},
+        {'0', '0', '0', '0', '0', '0'},
+        {'0', '0', '0', '0', '0', '0'},
+        {'0', '0', '0', '0', '0', '0'},
+        {'0', '0', '0', '0', '0', '0'}
     };
-    
-    while (continuar =='S'){
-        //VERIAVEL QUE PRECISA SER REDEFINIDA A CADA CICLO
-        int lin = 1;
-        int verificacaofileira = 0;
-        bool onibusCheio = true;
-        
-        //CABEÇALHO
-        printf("\n---------------------------------------------------------------------");
-        printf("\n      FILEIRA");
-        printf("\n   A  B  C  D  E");
-        printf("\n");
-        
-        //imprimir matriz
-        for (int i = 0; i < 5; ++i) {
+
+
+int imprimirMatriz(){
+    int lin = 1;
+    onibusCheio = true;
+
+    printf("\n        FILEIRA");
+    printf("\n   A  B  C  D  E  F");
+    printf("\n");
+
+    for (int i = 0; i < 5; ++i) {
             printf("%i", lin);
             printf("|");
             lin = lin + 1;
-            for (int j = 0; j < 5; ++j) {
+            for (int j = 0; j < 6; ++j) {
                 if(matriz[i][j] != 'X'){
                     onibusCheio= false;
                 }
@@ -50,10 +48,33 @@ int main() {
             }
             printf("\n");
         };
-
-        //legenda
         printf("\n Poltrona reservada = X |");
-        printf(" Poltrona livre = 0");
+        printf(" Poltrona livre = 0\n");
+        return onibusCheio;
+}
+
+int encerrarSistema(){
+    printf("\nDeseja reservar outra poltrona? (S/N) ");
+    scanf(" %c", &continua);
+    continua = toupper(continua);
+    system("cls");
+    return continua;
+}
+
+int main() {
+
+    
+
+    while (continua =='S'){
+        //VERIAVEL QUE PRECISA SER REDEFINIDA A CADA CICLO
+        int verificacaofileira = 0;
+        
+        
+        //CABEÇALHO
+        printf("\n---------------------------------------------------------------------");
+        
+        //imprimir matriz
+        imprimirMatriz();
         
         printf("\n---------------------------------------------------------------------");
 
@@ -63,8 +84,29 @@ int main() {
             break;
         };
         
+
+        printf("\n Escolha sua classe: ");
+        printf("\n [1] Economica ");
+        printf("\n [2] Executiva ");
+
+        scanf("%i", classeReserva);
+
+        switch (classeReserva)
+        {
+        case 1:
+            printf("\n Digite a fileira desejada (B-E): ");
+            break;
+        
+        case 2:
+            printf("\n Digite a fileira desejada (A-F): ");
+            break;
+        default:
+            break;
+        }
+        
+
         //VERIFICAR FILEIRA
-        printf("\n Digite a fileira desejada (A-E): ");
+        printf("\n Digite a fileira desejada (A-F): ");
         scanf( " %c", &fileira);
         
         //transformar de minuscula para maiuscula
@@ -131,8 +173,19 @@ int main() {
 
         }else{
             matriz[poltrona][col] = 'X';
-            printf("\033[30;42m Poltrona reservada: %c%i \033[0m",fileira,poltrona+1);
+            imprimirMatriz();
+            printf("\n\033[30;42m Poltrona reservada: %c%i \033[0m\n",fileira,poltrona+1);
+            encerrarSistema();
+
+            if (continua != 'S' && continua != 'N'){
+                system("cls");
+                printf("\033[37;41mOpcao invalida... \033[0m");
+                encerrarSistema();
+            };
         }
     }
+
+    printf("\033[30;42mObrigado pela preferencia! Tenha um boa viagem...\033[0m\n");
+
     return 0;
 }
